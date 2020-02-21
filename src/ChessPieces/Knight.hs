37c0a27eaff36cf1a1f13f26deeffcd5ce53c387
Collin Duncan <cgduncan7@gmail.com>
2020-02-21 09:45:45 +0100

    added valid move checks

--- src/ChessPieces/Knight.hs
+++ src/ChessPieces/Knight.hs
@@ -4,5 +4,22 @@ module ChessPieces.Knight (
 
 import ChessTypes
 
-isValidMove :: String -> Location -> Bool
-isValidMove m l = False
+isValidMove :: MoveData -> Location -> Bool
+isValidMove (_, _, KCastle) (c, r) = False
+isValidMove (_, _, QCastle) (c, r) = False
+isValidMove (_, Nothing, MoveLocation (x, y)) (c, r)
+  | diff == (2,1)     = True
+  | diff == (2, -1)   = True
+  | diff == (1, 2)    = True
+  | diff == (1, -2)   = True
+  | diff == (-1, 2)   = True
+  | diff == (-1, -2)  = True
+  | diff == (-2, 1)   = True
+  | diff == (-2, -1)  = True
+  | otherwise = False
+  where colDiff = (fromEnum x) - (fromEnum c)
+        rowDiff = (fromEnum y) - (fromEnum r)
+        diff = (colDiff, rowDiff)
+
+isValidMove (_, Just (Left s), MoveLocation (x, y)) (c, r) = c == s
+isValidMove (_, Just (Right s), MoveLocation (x, y)) (c, r) = r == s
