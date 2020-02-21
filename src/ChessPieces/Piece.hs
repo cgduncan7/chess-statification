e7fbe905c487c9825415e570540a109918d2f077
Collin Duncan <cgduncan7@gmail.com>
2020-02-21 09:46:22 +0100

    methods to get piece data

--- src/ChessPieces/Piece.hs
+++ src/ChessPieces/Piece.hs
@@ -1,6 +1,13 @@
 module ChessPieces.Piece (
   PieceRank(..),
   Piece(..),
+  getPieceRank,
+  getPieceLocation,
+  getPieceColor,
+  getPiecesByColor,
+  getPiecesByRank,
+  getPiecesByColorAndRank,
+  getValidPieceForMove,
   isValidMove
 ) where
 
@@ -8,12 +15,38 @@ import ChessTypes
 import qualified ChessPieces.Pawn as Pawn
 import qualified ChessPieces.Knight as Knight
 
-data PieceRank = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Read, Eq)
-
 data Piece = Piece PieceRank Location Color deriving (Show, Read, Eq)
 
-isValidMove :: String -> Piece -> Bool
-isValidMove s (Piece p l _) = 
+getPieceRank :: Piece -> PieceRank
+getPieceRank (Piece r _ _) = r
+
+getPieceLocation :: Piece -> Location
+getPieceLocation (Piece _ l _) = l
+
+getPieceColor :: Piece -> Color
+getPieceColor (Piece _ _ c) = c
+
+getPiecesByColor :: Color -> [Piece] -> [Piece]
+getPiecesByColor c p =
+  let match x = (getPieceColor x) == c
+  in filter match p
+
+getPiecesByRank :: [PieceRank] -> [Piece] -> [Piece]
+getPiecesByRank r p =
+  let match x = (getPieceRank x) `elem` r
+  in filter match p
+
+getPiecesByColorAndRank :: [Piece] -> Color -> [PieceRank] -> [Piece]
+getPiecesByColorAndRank p c r = getPiecesByColor c (getPiecesByRank r p)
+
+getValidPieceForMove :: MoveData -> Color -> [Piece] -> Piece
+getValidPieceForMove (rs, s, m) c p =
+  let avail = getPiecesByColorAndRank p c rs
+      isValid x = isValidMove (rs, s, m) x
+  in filter isValid avail !! 0
+
+isValidMove :: MoveData -> Piece -> Bool
+isValidMove m (Piece p l _) = 
   case p of
-    Pawn   -> Pawn.isValidMove s l
-    Knight -> Knight.isValidMove s l
+    Pawn   -> Pawn.isValidMove m l
+    Knight -> Knight.isValidMove m l
